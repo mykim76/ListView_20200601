3a5c8ccd54ae97cb41fefcd0fbfa9c60fdf1ea9d
mykim76 <iyoungfm@gmail.com>
2020-06-01 21:48:48 +0900

    목록변수 ArrayList를 만들고 실 데이터 추가

--- app/src/main/java/com/example/listview_20200601/MainActivity.kt
+++ app/src/main/java/com/example/listview_20200601/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.listview_20200601
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listview_20200601.datas.Student
 import com.example.listview_20200601.BaseActivity as BaseActivity1
 
 class MainActivity : BaseActivity1() {
+
+    val students = ArrayList<Student>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +21,9 @@ class MainActivity : BaseActivity1() {
     }
 
     override fun setValues() {
+        students.add(Student("조경진",1988,false))
+        students.add(Student("김미영",2010,false))
+        students.add(Student("김성우",1982,true))
+        students.add(Student("김형근",1998,true))
     }
 }
