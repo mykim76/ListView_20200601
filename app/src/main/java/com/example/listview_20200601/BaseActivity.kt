7533cc6a1ddaa44e100c840d750bc5c6aed876f5
mykim76 <iyoungfm@gmail.com>
2020-06-01 20:45:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/listview_20200601/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.listview_20200601
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+    
+}
